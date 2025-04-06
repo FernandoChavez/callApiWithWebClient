70a471bd32c98fc6163d54f47cd6a6ab0fb47ad4
FuerzaDON <ingluisfernandochavez@hotmail.com>
2025-04-06 02:51:30 -0600

    defaultHeader added to webClient bean

--- src/main/java/com/fuerzadon/web_client_spring__boot/config/ConfigWebClient.java
+++ src/main/java/com/fuerzadon/web_client_spring__boot/config/ConfigWebClient.java
@@ -2,6 +2,8 @@ package com.fuerzadon.web_client_spring__boot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -11,7 +13,10 @@ public class ConfigWebClient {
 
     @Bean
     WebClient webClient(){
-        return WebClient.builder().baseUrl(url).build();
+        return WebClient.builder()
+                .baseUrl(url)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 
 }
